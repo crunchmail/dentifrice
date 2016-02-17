4129661a55ef96d600f2d2ee597d1dfcba2efcb5
Nicolas Brisac <nbrisac@oasiswork.fr>
2016-02-17 10:11:37 +0100

    reindent with 4 ts

--- src/app/init-functions.js
+++ src/app/init-functions.js
@@ -8,54 +8,54 @@
 // http://mozilla.org/MPL/2.0/.
 
 /**
- * Post Message settings
- */
+* Post Message settings
+*/
 var
-  msgPrefix    = "[Dentifrice]",
-  msgPrefixLen = msgPrefix.length,
-  title,
-  lang,
-  appRootUrl;
+msgPrefix    = "[Dentifrice]",
+msgPrefixLen = msgPrefix.length,
+title,
+lang,
+appRootUrl;
 
 /**
- * Log error messages to console.
- * Always logged regardless of the 'log' setting.
- */
+* Log error messages to console.
+* Always logged regardless of the 'log' setting.
+*/
 function error (msg) {
-  output('error', msg, true);
+    output('error', msg, true);
 }
 
 /**
- * Log warning messages to console.
- * Always logged regardless of the 'log' setting.
- */
+* Log warning messages to console.
+* Always logged regardless of the 'log' setting.
+*/
 function warn (msg) {
-  output('warn', msg, true);
+    output('warn', msg, true);
 }
 
 /**
- * Log information messages to console.
- * Only displayed if 'log' setting is true.
- */
+* Log information messages to console.
+* Only displayed if 'log' setting is true.
+*/
 function info (msg) {
-  output('info', msg, settings.log);
+    output('info', msg, settings.log);
 }
 
 /**
- * Log debug messages to console.
- * Only displayed if 'debug' setting is true.
- */
+* Log debug messages to console.
+* Only displayed if 'debug' setting is true.
+*/
 function debug (msg) {
     output('log', msg, settings.debug);
 }
 
 /**
- * Print messages to the console using provided level.
- */
+* Print messages to the console using provided level.
+*/
 function output (type, msg, enabled) {
-  if (true === enabled && 'object' === typeof window.console) {
-    console[type]('DentifriceEditor: ' + msg);
-  }
+    if (true === enabled && 'object' === typeof window.console) {
+        console[type]('DentifriceEditor: ' + msg);
+    }
 }
 
 /*
@@ -95,165 +95,165 @@ var blocksIcons = {
 };
 
 /**
- * Load settings
- * See _local_settings.js and README.md for how to override
- */
+* Load settings
+* See _local_settings.js and README.md for how to override
+*/
 var settings = {},
-    CKEDITOR_BASEPATH;
+CKEDITOR_BASEPATH;
 function loadSettings (local_settings) {
-  var default_settings = {
-    log          : true,
-    debug        : false,
-    showSpinner  : true,
-    noinlineAttr : 'inline="false"',
-    uploadURL    : 'http://localhost:8080/files',
-    plugins      : {
-      'draftStore'   : 'drafts-localstore.js',
-      'uploadStore'  : 'upload-simplestore.js',
-      'beforeEditor' : [],
-      'afterEditor'  : []
-    }
-  };
-  // Merge local_settings and defaults
-  settings = _.merge(default_settings, local_settings);
-  // Force ckeditor BasePath
-  CKEDITOR_BASEPATH = settings.appRootUrl + '/ckeditor/';
+    var default_settings = {
+        log          : true,
+        debug        : false,
+        showSpinner  : true,
+        noinlineAttr : 'inline="false"',
+        uploadURL    : 'http://localhost:8080/files',
+        plugins      : {
+            'draftStore'   : 'drafts-localstore.js',
+            'uploadStore'  : 'upload-simplestore.js',
+            'beforeEditor' : [],
+            'afterEditor'  : []
+        }
+    };
+    // Merge local_settings and defaults
+    settings = _.merge(default_settings, local_settings);
+    // Force ckeditor BasePath
+    CKEDITOR_BASEPATH = settings.appRootUrl + '/ckeditor/';
 }
 
 /* Helper function to get a file's absolute URL
- * using discovered appRootUrl
- */
+* using discovered appRootUrl
+*/
 var getAbsoluteUrl = function (url) {
-  var r = new RegExp('^(?:[a-z]+:)?//', 'i');
-  if ( r.test(url) ) {
-   // URL is absolute, return unchanged
-   return url;
-  } else {
-   // URL is relative, concatenate with appRootUrl and return
-   return settings.appRootUrl + url.replace(/^\//, '');
-  }
+    var r = new RegExp('^(?:[a-z]+:)?//', 'i');
+    if ( r.test(url) ) {
+        // URL is absolute, return unchanged
+        return url;
+    } else {
+        // URL is relative, concatenate with appRootUrl and return
+        return settings.appRootUrl + url.replace(/^\//, '');
+    }
 };
 
 /* Spinner overlay
- */
+*/
 var spinner = function (action) {
-  var _overlay = $('.overlay');
-  var _spinner = $('.spinner');
-  var _spinnerLeft = (window.innerWidth/2) - (_spinner.width()/2);
-  _spinner.css('left',_spinnerLeft);
-  if (settings.showSpinner) {
-    switch (action) {
-      case 'show':
-        debug('Showing spinner');
-        //$('body').append(_spinner);
-        _overlay.show();
-        _spinner.show();
-        break;
-      case 'hide':
-        debug('Hiding spinner');
-        //_spinner.remove();
-        _spinner.hide();
-        _overlay.hide();
-        break;
-      default:
-        warn('Unknown spinner action');
+    var _overlay = $('.overlay');
+    var _spinner = $('.spinner');
+    var _spinnerLeft = (window.innerWidth/2) - (_spinner.width()/2);
+    _spinner.css('left',_spinnerLeft);
+    if (settings.showSpinner) {
+        switch (action) {
+            case 'show':
+            debug('Showing spinner');
+            //$('body').append(_spinner);
+            _overlay.show();
+            _spinner.show();
+            break;
+            case 'hide':
+            debug('Hiding spinner');
+            //_spinner.remove();
+            _spinner.hide();
+            _overlay.hide();
+            break;
+            default:
+            warn('Unknown spinner action');
+        }
     }
-  }
 };
 
 function _init (local_settings) {
-  // Add a few values to local settings before loading
-  local_settings.appRootUrl = appRootUrl;
-  local_settings.lang = lang;
-  local_settings.title = title;
-  // Get the settings
-  loadSettings(local_settings);
-  // And start initialising
-  dtfInit.loadEditor(dtfInit.loadI18n(function () {
-    // Load editor
-    info('Initializing editor');
-    dtfEditor.load();
-    // Hide spinner
-    spinner('hide');
-  }));
+    // Add a few values to local settings before loading
+    local_settings.appRootUrl = appRootUrl;
+    local_settings.lang = lang;
+    local_settings.title = title;
+    // Get the settings
+    loadSettings(local_settings);
+    // And start initialising
+    dtfInit.loadEditor(dtfInit.loadI18n(function () {
+        // Load editor
+        info('Initializing editor');
+        dtfEditor.load();
+        // Hide spinner
+        spinner('hide');
+    }));
 }
 
 function _getQueryParameterByName (name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 /* Some global variables that we will need
- * throughout the application
- */
+* throughout the application
+*/
 var blocks_config = { styles: {} };
 
 
 var dtfInit = (function ( $ ) {
-  'use strict';
+    'use strict';
 
-  var loadScripts = function (scripts, index, callback, failCallback) {
-    if (typeof scripts === 'string') scripts = [scripts];
-    debug('Loading script: ' + scripts[index]);
-    $.getScript(scripts[index], function () {
-      if(index + 1 <= scripts.length - 1) {
-        loadScripts(scripts, index + 1, callback, failCallback);
-      } else {
-        if(callback) callback();
-      }
-    })
-    .fail(function(jqxhr, settings, exception) {
-      warn(scripts[index] + ' loading error: ' + exception);
-      if(failCallback) failCallback();
-    });
-  };
+    var loadScripts = function (scripts, index, callback, failCallback) {
+        if (typeof scripts === 'string') scripts = [scripts];
+        debug('Loading script: ' + scripts[index]);
+        $.getScript(scripts[index], function () {
+            if(index + 1 <= scripts.length - 1) {
+                loadScripts(scripts, index + 1, callback, failCallback);
+            } else {
+                if(callback) callback();
+            }
+        })
+        .fail(function(jqxhr, settings, exception) {
+            warn(scripts[index] + ' loading error: ' + exception);
+            if(failCallback) failCallback();
+        });
+    };
 
-  var loadI18n = function (callback) {
-      // Load translations
-      info('Initializing locale');
-      $.i18n.init({
-        lng             : settings.lang,
-        fallbackLng     : 'en',
-        lowerCaseLng    : true,
-        resGetPath      : settings.appRootUrl + 'locales/__lng__.json',
-        useLocalStorage : false,
-        debug           : settings.debug
-      }, function () {
-        if(callback) callback();
-      });
-  };
+    var loadI18n = function (callback) {
+        // Load translations
+        info('Initializing locale');
+        $.i18n.init({
+            lng             : settings.lang,
+            fallbackLng     : 'en',
+            lowerCaseLng    : true,
+            resGetPath      : settings.appRootUrl + 'locales/__lng__.json',
+            useLocalStorage : false,
+            debug           : settings.debug
+        }, function () {
+            if(callback) callback();
+        });
+    };
 
-  var loadEditor = function (callback) {
-    // Show spinner
-    spinner('show');
-    // Build list of scripts to load
-    var scriptsList = [
-      settings.appRootUrl + 'js/plugins/' + settings.plugins.draftStore,
-      settings.appRootUrl + 'js/plugins/' + settings.plugins.uploadStore
-    ];
-    // Add before-editor custom scripts
-    if (typeof settings.plugins.beforeEditor === 'string') settings.plugins.beforeEditor = [settings.plugins.beforeEditor];
-    var beforeEditor = settings.plugins.beforeEditor.map( function(item) { return getAbsoluteUrl(item); });
-    scriptsList = scriptsList.concat(beforeEditor);
-    // Add the editor itself
-    scriptsList.push(settings.appRootUrl + 'js/editor.js');
-    // Add after-editor custom scripts
-    if (typeof settings.plugins.afterEditor === 'string') settings.plugins.afterEditor = [settings.plugins.afterEditor];
-    var afterEditor = settings.plugins.afterEditor.map( function(item) { return getAbsoluteUrl(item); });
-    scriptsList = scriptsList.concat(afterEditor);
+    var loadEditor = function (callback) {
+        // Show spinner
+        spinner('show');
+        // Build list of scripts to load
+        var scriptsList = [
+            settings.appRootUrl + 'js/plugins/' + settings.plugins.draftStore,
+            settings.appRootUrl + 'js/plugins/' + settings.plugins.uploadStore
+        ];
+        // Add before-editor custom scripts
+        if (typeof settings.plugins.beforeEditor === 'string') settings.plugins.beforeEditor = [settings.plugins.beforeEditor];
+        var beforeEditor = settings.plugins.beforeEditor.map( function(item) { return getAbsoluteUrl(item); });
+        scriptsList = scriptsList.concat(beforeEditor);
+        // Add the editor itself
+        scriptsList.push(settings.appRootUrl + 'js/editor.js');
+        // Add after-editor custom scripts
+        if (typeof settings.plugins.afterEditor === 'string') settings.plugins.afterEditor = [settings.plugins.afterEditor];
+        var afterEditor = settings.plugins.afterEditor.map( function(item) { return getAbsoluteUrl(item); });
+        scriptsList = scriptsList.concat(afterEditor);
 
-    // Dynamically load scripts
-    loadScripts(scriptsList, 0, function () {
-      if(callback) callback();
-    });
-  };
+        // Dynamically load scripts
+        loadScripts(scriptsList, 0, function () {
+            if(callback) callback();
+        });
+    };
 
-  return {
-    loadScripts : loadScripts,
-    loadI18n    : loadI18n,
-    loadEditor  : loadEditor
-  };
+    return {
+        loadScripts : loadScripts,
+        loadI18n    : loadI18n,
+        loadEditor  : loadEditor
+    };
 
 })( jQuery );
